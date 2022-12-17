f3c6b18cfbf0ab80f4e7e386664240a1f8afe7c1
Ybaedbaenn <84321760+Ybaedbaenn@users.noreply.github.com>
2022-12-17 21:57:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const context = JSON.parse(document.getElementById("context").textContent);
-if (context.bad_URL) {
+if (context.bad_link) {
   let error_message = document.createElement("p");
   let item = document.getElementById("form");
-  let bad_URL = "https://my-l.ml" + decodeURIComponent(context.bad_URL).slice(0, -1);
+  let bad_link = decodeURIComponent(context.bad_link);
+  if (bad_link.slice(-1) == "/") {
+    let bad_URL = "https://my-l.ml" + bad_link.slice(0, -1);
+  } else {
+    let bad_URL = "https://my-l.ml" + bad_link;
+  }
   let error_message = "The requested link: "+ bad_URL + " does not exist.";
   document.body.insertBefore(error_message, item);
   error_message.innerText = decodeURIComponent(context.bad_URL);
