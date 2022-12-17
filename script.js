2a107713e8eaf7820a4afd86081968a17a569360
Ybaedbaenn <84321760+Ybaedbaenn@users.noreply.github.com>
2022-12-17 21:00:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const context = JSON.parse(document.getElementById("context").textContent);
 error_message = document.getElementById("error_message");
-if (context.bad_URL){
+if (context.bad_URL) {
   error_message.innerText = decodeURIComponent(context.bad_URL);
+} else {
+   error_message.remove();
 }
