e8e2c338ad8b3923700d9e46963b009c705b9eb0
Ybaedbaenn <84321760+Ybaedbaenn@users.noreply.github.com>
2022-12-17 22:02:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const context = JSON.parse(document.getElementById("context").textContent);
-if (context.bad_link) {
-  let error_message_paragraph = document.createElement("p");
-  let item = document.getElementById("form");
-  let bad_link = decodeURIComponent(context.bad_link);
-  if (bad_link.slice(-1) == "/") {
-    let bad_URL = "https://my-l.ml" + bad_link.slice(0, -1);
-  } else {
-    let bad_URL = "https://my-l.ml" + bad_link;
+function initialize() {
+  const context = JSON.parse(document.getElementById("context").textContent);
+  if (context.bad_link) {
+    let error_message_paragraph = document.createElement("p");
+    let item = document.getElementById("form");
+    let bad_link = decodeURIComponent(context.bad_link);
+    if (bad_link.slice(-1) == "/") {
+      var bad_URL = "https://my-l.ml" + bad_link.slice(0, -1);
+    } else {
+      var bad_URL = "https://my-l.ml" + bad_link;
+    }
+    let error_message = "The requested link: "+ bad_URL + " does not exist.";
+    error_message_paragraph.innerText = error_message;
+    document.body.insertBefore(error_message, item);
   }
-  let error_message = "The requested link: "+ bad_URL + " does not exist.";
-  error_message_paragraph.innerText = error_message;
-  document.body.insertBefore(error_message, item);
 }
